12c9d46ae30a7e1a7cf1d1227de7a4d15302cd87
Sivakumar Appakkudal Ravichandran <sivakumar.r17@ad.infosys.com>
2020-05-26 13:30:42 +0530

    update

---
+++ db.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    host: "sivademoserver.mysql.database.azure.com", 
+    user: "myroot@sivademoserver", 
+    password: 'Azure@12345', 
+    database: 'restful_db', 
+    port: 3306,
+    ssl: true
+});
+
+let restfuldb = {};
+
+restfuldb.all = () => {
+    return new Promise((resolve, reject) => {
+        pool.query(`Select * from product`, (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    });
+};
+
+restfuldb.one = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`Select * from product where product_id=?`, [id], (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            return resolve(results[0]);
+        });
+    });
+};
+
+module.exports = restfuldb;
---
+++ routes/products.js
@@ -0,0 +1,23 @@
+var express = require('express');
+const db = require('../db');
+
+var router = express.Router();
+
+// /* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send({message: 'I am Users View'});
+});
+
+// router.get('/', async(req, res, next) => {
+//   try {
+//       let results = await db.all();
+//       //res.json(results);
+//       res.send(results);
+//   } catch(e) {
+//       console.log(e);
+//       res.sendStatus(500);
+//   }
+
+// });
+
+module.exports = router;
