084db2114fe68d4a9c3f9927d3fcea16b3df8cd4
Sivakumar Appakkudal Ravichandran <sivakumar.r17@ad.infosys.com>
2020-05-26 14:19:30 +0530

    update

--- routes/products.js
+++ routes/products.js
@@ -1,23 +1,23 @@
 var express = require('express');
-//const db = require('../db');
+const db = require('../db');
 
 var router = express.Router();
 
 // /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send({message: 'I am Users View'});
-});
-
-// router.get('/', async(req, res, next) => {
-//   try {
-//       let results = await db.all();
-//       //res.json(results);
-//       res.send(results);
-//   } catch(e) {
-//       console.log(e);
-//       res.sendStatus(500);
-//   }
-
+// router.get('/', function(req, res, next) {
+//   res.send({message: 'I am Users View123'});
 // });
+
+router.get('/', async(req, res, next) => {
+  try {
+      let results = await db.all();
+      //res.json(results);
+      res.send(results);
+  } catch(e) {
+      console.log(e);
+      res.sendStatus(500);
+  }
+
+});
 
 module.exports = router;
